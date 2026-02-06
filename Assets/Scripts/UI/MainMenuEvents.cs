9f3e78cfcf2fd4e6d63a30e595012e1e7fd849f9
Jack Johnson <jrj221@byu.edu>
2026-02-05 19:21:53 -0700

    made a MainMenuEvents script which locks player and camera movement until you press start, then disables the menu ui

--- Assets/Scripts/UI/MainMenuEvents.cs
+++ Assets/Scripts/UI/MainMenuEvents.cs
@@ -7,9 +7,13 @@ public class MainMenuEvents : MonoBehaviour
 {
     private UIDocument _document;
     private Button _startButton;
+    public StateMachine playerMovement;
+    public CameraController cameraMovement;
 
     private void Awake()
     {
+        playerMovement.enabled = false;
+        cameraMovement.enabled = false;
         _document = GetComponent<UIDocument>();
         _startButton = _document.rootVisualElement.Q("StartGame") as Button; // type casts into a Button
 
@@ -27,5 +31,8 @@ public class MainMenuEvents : MonoBehaviour
     private void OnStartGameClick(ClickEvent e)
     {
         Debug.Log("play!");
+        playerMovement.enabled = true;
+        cameraMovement.enabled = true;
+        _document.enabled = false;
     }
 }
