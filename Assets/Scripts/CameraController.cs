5fe6961c76fb7be47f9404d0ed27bcf5ac11c6a1
Jack Johnson <jrj221@byu.edu>
2026-02-07 21:51:05 -0700

    refactored CameraController to no longer track inputs itself, instead relying on the input manager

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,50 +1,20 @@
-using System.IO.Pipes;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {   
     public GameObject player;
     public StateMachine controller;
-    public InputActionReference look;
-    private Vector2 deltaCameraMovement;
 
     public float sensitivity; // 1 works good. Higher values are more sensitive
     public float cameraMovementSmoothingSpeed;
     private float pitch;
     private float yaw;
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        // Cursor.lockState = CursorLockMode.Locked;
-    }
-
-
-    void OnEnable()
-    {
-        look.action.Enable();
-        look.action.performed += PerformLook;
-    }
-
-
-    void OnDisable()
-    {
-        look.action.Disable();
-        look.action.performed -= PerformLook;
-    }
+    public InputManager inputManager;
 
 
     void LateUpdate()
     {
         MoveCamera();
-    }
-
-
-    void PerformLook(InputAction.CallbackContext ctx)
-    {
-        deltaCameraMovement = ctx.ReadValue<Vector2>();
     }
 
 
@@ -55,7 +25,7 @@ public class CameraController : MonoBehaviour
         else transform.position = player.transform.position + new Vector3(0f, 0.4f, 0f);
 
         // player yaw
-        yaw += deltaCameraMovement.x * sensitivity;
+        yaw += inputManager.DeltaCameraMovement.x * sensitivity;
         Vector3 currPlayerRotation = player.transform.eulerAngles;
         player.transform.eulerAngles = new Vector3(currPlayerRotation.x, yaw, currPlayerRotation.z);
 
@@ -65,7 +35,7 @@ public class CameraController : MonoBehaviour
         
         // cam pitch
         float currPitch = pitch;
-        pitch += deltaCameraMovement.y * sensitivity;
+        pitch += inputManager.DeltaCameraMovement.y * sensitivity;
         pitch = Mathf.Clamp(pitch, -90f, 90f);
         transform.Rotate(currPitch - pitch, 0f, 0f);
     }
