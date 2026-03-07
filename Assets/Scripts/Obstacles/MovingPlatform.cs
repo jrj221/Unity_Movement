6cd9980bda15b53d9b236ddd5f9f75e49d6794d8
Jack Johnson <jrj221@byu.edu>
2026-03-07 14:50:43 -0700

    Created script to make platforms oscillate back and forth between two points. Adjustable move speed and how far it moves

---
+++ Assets/Scripts/Obstacles/MovingPlatform.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    private Vector3 _pointOne;
+    private Vector3 _pointTwo;
+    [SerializeField] private float _movingRadius;
+    [SerializeField] private float _moveSpeed;
+
+    private void Awake()
+    {
+        _pointOne = new Vector3(transform.position.x - _movingRadius, transform.position.y, transform.position.z);
+        _pointTwo = new Vector3(transform.position.x + _movingRadius, transform.position.y, transform.position.z);
+    }
+
+    private void Update()
+    {
+        float t = Mathf.PingPong(Time.time * _moveSpeed + 0.5f, 1f);
+        transform.position = Vector3.Lerp(_pointOne,  _pointTwo, t);
+    }
+}
